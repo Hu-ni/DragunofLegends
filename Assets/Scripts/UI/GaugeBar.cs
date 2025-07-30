e179eb6b2d62dda6d4f4f1fc19e752852bb24575
muntory <38274669+muntory@users.noreply.github.com>
2025-07-30 17:30:14 +0900

    [feat] Add GaugeBar prefab

--- Assets/Scripts/UI/GaugeBar.cs
+++ Assets/Scripts/UI/GaugeBar.cs
@@ -1,77 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
-using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class GaugeBar : MonoBehaviour
 {
     [SerializeField]
-    bool hasIcon;
-    [SerializeField]
-    Sprite icon;
-    [SerializeField]
     Image fillImage;
     [SerializeField, Range(0f, 1f)]
     float value;
 
-    Transform iconTransform;
-    UpdatableTextUI updatableText;
-
     private void Awake()
     {
-        if (hasIcon && icon == null)
-        {
-            Debug.LogWarning("IconSprite 연결 해주세요.");
-            return;
-        }
-        
-        foreach (Transform t in GetComponentsInChildren<Transform>())
-        {
-            if (iconTransform && updatableText)
-            {
-                break;
-            }
-
-            if (t.name == "Icon")
-            {
-                iconTransform = t;
-                continue;
-            }
-            if (t.name == "UpdatableText")
-            {
-                updatableText = t.GetComponent<UpdatableTextUI>(); 
-                continue;
-            }
-        }
-
-        if (hasIcon)
-        {
-            iconTransform.GetComponent<Image>().sprite = icon;
-        }
-
         SetValue(value);
-        updatableText.UpdateText("Lv.1");
     }
 
-
-    private void OnEnable()
-    {
-        if (hasIcon)
-        {
-            updatableText.gameObject.SetActive(false);
-        }
-        else
-        {
-            iconTransform.gameObject.SetActive(false);
-        }
-
-    }
-
-    /// <summary>
-    /// 
-    /// </summary>
-    /// <param name="value"></param>
     public void SetValue(float value)
     {
         if (fillImage == null) return;
@@ -80,6 +23,4 @@ public class GaugeBar : MonoBehaviour
         fillImage.fillAmount = value;
     }
 
-
-    
 }
