f5dcee046a308c4a4596727bbf244fec4e470a33
박상현 <night8641@naver.com>
2025-07-31 09:47:20 +0900

    weapondata

---
+++ Assets/Scripts/Weapon/WeaponType.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponType : MonoBehaviour
+{
+public enum WeaponTypes
+    {
+        range,
+        melee,
+        chase
+    }
+}
