ffcc9f0f21b49ce2a5f05ddd0971958b15dca119
muntory <38274669+muntory@users.noreply.github.com>
2025-07-31 11:46:52 +0900

    [feat] Add 휴식 팝업 UI

---
+++ Assets/Scripts/UI/Popup/RestPopupUI.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RestPopupUI : PopupUI
+{
+    [SerializeField]
+    Button yesButton;
+    [SerializeField]
+    Button noButton;
+
+    private void Awake()
+    {
+
+        if (yesButton == null)
+        {
+            yesButton = transform.Find("YesButton").GetComponent<Button>();
+        }
+
+        if (noButton == null)
+        {
+            noButton = transform.Find("NoButton").GetComponent<Button>();
+        }
+    }
+
+
+    protected override void Start()
+    {
+        base.Start();
+
+        yesButton.onClick.AddListener(() =>
+        {
+            Debug.Log("회복 연결시켜야함");
+        });
+
+        noButton.onClick.AddListener(() =>
+        {
+            UIManager.Instance.ClosePopupUI();
+        });
+
+    }
+}
