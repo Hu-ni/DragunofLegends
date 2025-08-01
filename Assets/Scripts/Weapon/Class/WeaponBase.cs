1791745edb148ceb5d7de64025e5622fd1495f0d
박상현 <night8641@naver.com>
2025-08-01 17:11:11 +0900

    new weapon

---
+++ Assets/Scripts/Weapon/Class/WeaponBase.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class WeaponBase : MonoBehaviour
+{
+    protected int level = 0;
+
+    [SerializeField]
+    protected WeaponBase_SO weaponData;
+
+    protected float damage;
+    protected float cooldown;
+    protected float criticalChance;
+
+    protected Sprite weaponSprite;
+    protected Animator weaponAnimator;
+
+    protected float attackCooltime = 0f;
+
+    protected virtual void Update()
+    {
+        attackCooltime = Mathf.Clamp(attackCooltime - Time.deltaTime, 0, cooldown);
+
+    }
+
+    public virtual void SetWeaponStat(int level)
+    {
+        damage = weaponData.damages[level];
+        cooldown = weaponData.cooldowns[level];
+        criticalChance = weaponData.criticalChances[level];
+    }
+
+    public abstract void Attack();
+}
---
+++ Assets/Scripts/Weapon/ProjectileWeapon.cs
@@ -0,0 +1,93 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectileWeapon : WeaponBase
+{
+    protected int projectileCounts;
+    protected float projectileSpeeds;
+    protected int penetrationCounts;
+
+    [SerializeField] public GameObject projectilePrefab;
+    [SerializeField] public Transform startPostiion;
+    SpriteRenderer spriteRenderer;
+
+    private void Awake()
+    {
+        if (weaponData == null)
+        {
+            Debug.LogWarning("무기 데이터 연결 해주세요.");
+            return;
+
+        }
+
+        if (weaponSprite == null)
+        {
+            weaponSprite = weaponData.weaponSprites[0];
+        }
+
+        if (weaponAnimator == null)
+        {
+            weaponAnimator = weaponData.weaponAnimator;
+        }
+
+        if (spriteRenderer == null)
+        {
+            spriteRenderer = GetComponentInChildren<SpriteRenderer>();
+        }
+    }
+
+    private void Start()
+    {
+        SetWeaponStat(level);
+        spriteRenderer.sprite = weaponSprite;
+        attackCooltime = cooldown;
+    }
+
+    protected override void Update()
+    {
+        base.Update();
+
+        if (Input.GetKeyDown(KeyCode.Space)) //확인하기
+        {
+            Attack();
+        }
+    }
+
+    public override void SetWeaponStat(int level)
+    {
+        base.SetWeaponStat(level);
+
+        ProjectileWeapon_SO projectileWeaponData = weaponData as ProjectileWeapon_SO;
+
+        int projectileCounts = projectileWeaponData.projectileCounts[level];
+        float projectileSpeeds = projectileWeaponData.projectileSpeeds[level];
+        int penetrationCounts = projectileWeaponData.penetrationCounts[level];
+    }
+
+    public override void Attack()
+    {
+        if (attackCooltime > 0f)
+        {
+            return;
+        }
+
+        LayerMask targetLayer = LayerMask.GetMask("Enemy");
+
+        for (int i = 0; i < projectileCounts; i++)
+        {
+            CreateProjectile();
+
+            Debug.Log("발사");
+            attackCooltime = cooldown;
+
+        }
+    }
+
+    public void CreateProjectile()
+    {
+        GameObject obj = Instantiate(projectilePrefab,startPostiion.position, Quaternion.identity);
+        ProjectileController projectileController = obj.GetComponent<ProjectileController>();
+        projectileController.Init(direction, rangeWeaponHandler);
+    }
+}
