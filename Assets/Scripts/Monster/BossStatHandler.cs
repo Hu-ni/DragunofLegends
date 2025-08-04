73650f84d23e682a9a9e0f23926a8b3c4a812c2d
0time12 <onr55@naver.com>
2025-08-04 14:21:59 +0900

    [Feat] 보스 무적시간 추가

--- Assets/Scripts/Monster/BossStatHandler.cs
+++ Assets/Scripts/Monster/BossStatHandler.cs
@@ -9,20 +9,40 @@ public class BossStatHandler : MonoBehaviour
     public float chargeDamage = 30;
     public float rangedDamage = 15;
     public float contecDamage = 10;
+    [SerializeField]
+    private float invincibilityDuration = 0.5f;
+    private float invincibilityTimer = 0f;
 
-
-    void Awake()
+    private void Start()
     {
         currentHealth = maxHealth;
+    }
+    private void Update()
+    {
+        if (invincibilityTimer > 0)
+        {
+            invincibilityTimer -= Time.deltaTime;
+        }
     }
 
     public void TakeDamage(float damage)
     {
+        // 무적 시간이 아닐 때만 데미지를 입도록
+        if (invincibilityTimer > 0)
+        {
+            Debug.Log("Player is invincible!");
+            return;
+        }
+
         currentHealth -= damage;
+        Debug.Log("Player took " + damage + " damage. Current health: " + currentHealth);
+
+        // 데미지를 입은 후 무적 시간 시작
+        invincibilityTimer = invincibilityDuration;
+
         if (currentHealth <= 0)
         {
-            // 보스 사망 처리 로직
-            Debug.Log("보스가 죽었습니다.");
+            // 죽음 애니메이션
         }
     }
 }
